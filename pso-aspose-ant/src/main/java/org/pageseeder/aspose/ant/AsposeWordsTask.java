a3886d86fbd8c3a4143167bb0e689a5915a53a99
Philip Rutherford <philipr@weborganic.com>
2026-06-22 16:59:24 +1000

    Feature: Added support for appending DOCX files

--- pso-aspose-ant/src/main/java/org/pageseeder/aspose/ant/AsposeWordsTask.java
+++ pso-aspose-ant/src/main/java/org/pageseeder/aspose/ant/AsposeWordsTask.java
@@ -10,7 +10,6 @@ import org.apache.tools.ant.Task;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import com.aspose.words.cloud.*;
 import com.aspose.words.cloud.api.*;
@@ -18,6 +17,7 @@ import com.aspose.words.cloud.model.*;
 import com.aspose.words.cloud.model.requests.*;
 import com.aspose.words.cloud.model.responses.*;
 import java.nio.file.Files;
+import java.util.UUID;
 
 /**
  * An ANT task to convert a DOCX file to PDF using Aspose cloud
@@ -26,6 +26,7 @@ import java.nio.file.Files;
  */
 public final class AsposeWordsTask extends Task {
 
+  private static final String DOCX_EXTENSION = ".docx";
   /**
    * The Word document to convert.
    */
@@ -35,21 +36,6 @@ public final class AsposeWordsTask extends Task {
    * The PDF file to create.
    */
   private File destination;
-
-  /**
-   * The name of the working directory
-   */
-  private File working;
-
-  /**
-   * The configuration.
-   */
-  private File config;
-
-  /**
-   * List of parameters specified for the transformation into PSML
-   */
-  private List<Parameter> params = new ArrayList<>();
 
   /**
    * The Aspose cloud base URL
@@ -65,6 +51,11 @@ public final class AsposeWordsTask extends Task {
    * The Aspose cloud client secret
    */
   private String clientSecret;
+
+  /**
+   * Pipe-separated list of document names to append to the source document.
+   */
+  private String appendDocs = null;
 
   /**
    * Whether to update DOCX fields
@@ -83,7 +74,7 @@ public final class AsposeWordsTask extends Task {
     if (!(docx.exists())) throw new BuildException("the document " + docx.getName()+ " doesn't exist");
     if (docx.isDirectory()) throw new BuildException("the document " + docx.getName() + " can't be a directory");
     String name = docx.getName();
-    if (!name.endsWith(".docx")) {
+    if (!name.endsWith(DOCX_EXTENSION)) {
       log("Word document file should generally end with .docx - but was "+name);
     }
     this.source = docx;
@@ -120,6 +111,14 @@ public final class AsposeWordsTask extends Task {
   }
 
   /**
+   *
+   * @param appendDocs pipe-separated list of document names to append to the source document.
+   */
+  public void setAppendDocs(String appendDocs) {
+    this.appendDocs = appendDocs;
+  }
+
+  /**
    * @param update whether to update DOCX fields
    */
   public void setUpdateFields(boolean update) {
@@ -145,31 +144,125 @@ public final class AsposeWordsTask extends Task {
     ApiClient apiClient = new ApiClient(this.clientId, this.clientSecret, this.baseUrl);
     apiClient.setReadTimeout(1200_000); // 20 minutes
     WordsApi wordsApi = new WordsApi(apiClient);
-    try {
-      byte[] requestDocument = Files.readAllBytes(this.source.toPath());
-      long temp_folder = System.nanoTime();
-      if (this.updateFields) {
-        String temp_result = temp_folder + "/" + this.source.getName();
-        UpdateFieldsOnlineRequest request = new UpdateFieldsOnlineRequest(requestDocument,
-            null, null, null, false, temp_result);
-        UpdateFieldsOnlineResponse result = wordsApi.updateFieldsOnline(request);
-        requestDocument = result.getDocument().get(temp_result);
+    if (this.appendDocs != null) {
+      // 1. Generate a unique folder path for this specific thread execution
+      String uniqueFolder = "conversions/" + UUID.randomUUID().toString() + "/";
+      try {
+        appendAndConvert(uniqueFolder, wordsApi);
+      } catch (Exception ex) {
+        throw new BuildException(ex);
+      } finally {
+        // 7. Wipe the entire unique folder from the cloud to clean up all files at once
+        try {
+          log("Deleting conversion folder");
+          DeleteFolderRequest deleteFolder = new DeleteFolderRequest(uniqueFolder, null, true); // true = recursive delete
+          wordsApi.deleteFolder(deleteFolder);
+        } catch (Exception ex) {
+          log("Unable to delete conversion folder '" + uniqueFolder + "'", ex,
+                  org.apache.tools.ant.Project.MSG_WARN);
+        }
       }
-      PdfSaveOptionsData requestSaveOptionsData = new PdfSaveOptionsData();
-      String temp_result = temp_folder + "/" + this.destination.getName();
-      requestSaveOptionsData.setFileName(temp_result);
-      OutlineOptionsData outlineOptions = new OutlineOptionsData();
-      outlineOptions.headingsOutlineLevels(6);
-      requestSaveOptionsData.outlineOptions(outlineOptions);
-      SaveAsOnlineRequest request = new SaveAsOnlineRequest(requestDocument,
-          requestSaveOptionsData,null, null,null,false, null);
-      SaveAsOnlineResponse result = wordsApi.saveAsOnline(request);
-      Files.write(this.destination.toPath(),result.getDocument().get(temp_result));
-    } catch (IOException | MessagingException | ApiException ex) {
-      throw new BuildException(ex);
+    } else {
+      try {
+        if (convert(wordsApi)) return;
+      } catch (Exception ex) {
+        throw new BuildException(ex);
+      }
     }
-
     log("Conversion complete");
   }
 
+  private boolean convert(WordsApi wordsApi) throws IOException, ApiException, MessagingException {
+    byte[] requestDocument = Files.readAllBytes(this.source.toPath());
+    long tempFolder = System.nanoTime();
+    if (this.updateFields) {
+      log("Updating fields");
+      String tempResult = tempFolder + "/" + this.source.getName();
+      UpdateFieldsOnlineRequest request = new UpdateFieldsOnlineRequest(requestDocument,
+              null, null, null, false, tempResult);
+      UpdateFieldsOnlineResponse result = wordsApi.updateFieldsOnline(request);
+      requestDocument = result.getDocument().get(tempResult);
+    }
+    if (this.destination.getName().toLowerCase().endsWith(DOCX_EXTENSION)) {
+      Files.write(this.destination.toPath(), requestDocument);
+      return true;
+    }
+    PdfSaveOptionsData requestSaveOptionsData = new PdfSaveOptionsData();
+    String tempResult = tempFolder + "/" + this.destination.getName();
+    requestSaveOptionsData.setFileName(tempResult);
+    OutlineOptionsData outlineOptions = new OutlineOptionsData();
+    outlineOptions.headingsOutlineLevels(6);
+    requestSaveOptionsData.outlineOptions(outlineOptions);
+    SaveAsOnlineRequest request = new SaveAsOnlineRequest(requestDocument,
+            requestSaveOptionsData, null, null, null, false, null);
+    SaveAsOnlineResponse result = wordsApi.saveAsOnline(request);
+    Files.write(this.destination.toPath(), result.getDocument().get(tempResult));
+    return false;
+  }
+
+  private void appendAndConvert(String uniqueFolder, WordsApi wordsApi) throws IOException, ApiException, MessagingException {
+    log("Loading source document");
+    String masterName = this.source.getName();
+    byte[] masterBytes = Files.readAllBytes(this.source.toPath());
+    UploadFileRequest uploadMaster = new UploadFileRequest(masterBytes, uniqueFolder + masterName, null);
+    wordsApi.uploadFile(uploadMaster);
+
+    log("Loading append documents");
+    String[] appendFiles = this.appendDocs.split("\\|");
+    for (String appendFile : appendFiles) {
+      byte[] subBytes = Files.readAllBytes(new File(this.source.getParentFile(), appendFile).toPath());
+      // The second parameter specifies the path target inside the cloud
+      UploadFileRequest uploadSub = new UploadFileRequest(subBytes, uniqueFolder + appendFile, null);
+      wordsApi.uploadFile(uploadSub);
+    }
+
+    log("Appending documents");
+    ArrayList<DocumentEntry> entries = new ArrayList<>();
+    for (String subFile : appendFiles) {
+      DocumentEntry entry = new DocumentEntry();
+      // Crucial: Point to the file inside the unique folder
+      entry.setFileReference(new FileReference(uniqueFolder + subFile));
+      entry.setImportFormatMode(DocumentEntry.ImportFormatModeEnum.USEDESTINATIONSTYLES);
+      entries.add(entry);
+    }
+    DocumentEntryList documentList = new DocumentEntryList();
+    documentList.setDocumentEntries(entries);
+    AppendDocumentRequest appendRequest = new AppendDocumentRequest(
+             masterName, documentList, uniqueFolder, null, null,
+            null, null, false, null,
+            null, null);
+    wordsApi.appendDocument(appendRequest);
+
+    if (this.updateFields) {
+      log("Updating fields");
+      UpdateFieldsRequest request = new UpdateFieldsRequest(
+              masterName,
+              uniqueFolder,
+              null, null, null, null,
+              false, null);
+      wordsApi.updateFields(request);
+    }
+
+    String downloadPath = uniqueFolder + masterName;
+    if (!this.destination.getName().toLowerCase().endsWith(DOCX_EXTENSION)) {
+      log("Converting to PDF");
+      PdfSaveOptionsData saveOptions = new PdfSaveOptionsData();
+      saveOptions.setFileName("FinalCompiledBook.pdf");
+      OutlineOptionsData outlineOptions = new OutlineOptionsData();
+      outlineOptions.headingsOutlineLevels(6);
+      saveOptions.outlineOptions(outlineOptions);
+      SaveAsRequest saveAsRequest = new SaveAsRequest(
+              masterName, saveOptions, uniqueFolder,
+              null, null, null, null,
+              false, null);
+      wordsApi.saveAs(saveAsRequest);
+      downloadPath = uniqueFolder + "FinalCompiledBook.pdf";
+    }
+
+    log("Downloading result");
+    DownloadFileRequest downloadRequest = new DownloadFileRequest(
+            downloadPath,null,null);
+    byte[] resultBytes = wordsApi.downloadFile(downloadRequest);
+    Files.write(this.destination.toPath(), resultBytes);
+  }
 }
--- pso-aspose-ant/src/test/java/org/pageseeder/aspose/ant/AsposeWordsTaskTest.java
+++ pso-aspose-ant/src/test/java/org/pageseeder/aspose/ant/AsposeWordsTaskTest.java
@@ -3,7 +3,6 @@ package org.pageseeder.aspose.ant;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
 import java.io.*;
 import java.util.Properties;
@@ -17,34 +16,49 @@ public class AsposeWordsTaskTest {
   private static final File CLIENT = new File("aspose.properties");
 
   @Test
-  public void testBasic() throws IOException, SAXException {
-    testIndividual("basic", false);
+  public void testAppend() throws IOException {
+    testIndividual("append", true, "A heading 2.docx|Another heading 2.docx", false);
   }
 
   @Test
-  public void testPsGenerate() throws IOException, SAXException {
-    testIndividual("ps-generated", false);
+  public void testAppend2() throws IOException {
+    testIndividual("append2", true, "A heading 2.docx|Another heading 2.docx", true);
   }
 
   @Test
-  public void testUpdateFields() throws IOException, SAXException {
-    testIndividual("update-fields", true);
+  public void testBasic() throws IOException {
+    testIndividual("basic", false, null,  false);
   }
 
-  public void testIndividual(String folderName, boolean updateFields) throws IOException {
+  @Test
+  public void testPsGenerate() throws IOException {
+    testIndividual("ps-generated", false, null, false);
+  }
+
+  @Test
+  public void testUpdateFields() throws IOException {
+    testIndividual("update-fields", true, null, false);
+  }
+
+  @Test
+  public void testUpdateFields2() throws IOException {
+    testIndividual("update-fields2", true, null, true);
+  }
+
+  public void testIndividual(String folderName, boolean updateFields, String appendDocs, boolean outputDocx) throws IOException {
     File dir = new File(CASES, folderName);
     if (dir.isDirectory()) {
 
       if (new File(dir, dir.getName() + ".docx").exists()) {
         System.out.println(dir.getName());
-        File result_dir = new File(RESULTS, dir.getName());
-        if (result_dir.exists()) {
-          FileUtils.deleteDirectory(result_dir);
+        File resultDir = new File(RESULTS, dir.getName());
+        if (resultDir.exists()) {
+          FileUtils.deleteDirectory(resultDir);
         }
-        result_dir.mkdirs();
-        File result = new File(result_dir, dir.getName() + ".pdf");
-        File actual = process(dir, result, updateFields);
-        File expected = new File(dir, "expected.pdf");
+        Assert.assertTrue(resultDir.mkdirs());
+        File result = new File(resultDir, dir.getName() + (outputDocx ? ".docx" : ".pdf"));
+        File actual = process(dir, result, updateFields, appendDocs);
+        File expected = new File(dir, "expected" + (outputDocx ? ".docx" : ".pdf"));
 
         // Check that the files exist
         Assert.assertTrue(actual.exists());
@@ -52,7 +66,7 @@ public class AsposeWordsTaskTest {
 
         Assert.assertTrue(actual.length() > 0);
         Assert.assertTrue(expected.length() > 0);
-        Assert.assertTrue("Expected PDF file size:" + expected.length() + " but was " + actual.length(),
+        Assert.assertTrue("Expected result file size:" + expected.length() + " but was " + actual.length(),
             Math.abs(expected.length() - actual.length()) <= 30); // might be slight variation
       } else {
         throw new IOException("Unable to find DOCX file for test:" + dir.getName());
@@ -60,7 +74,7 @@ public class AsposeWordsTaskTest {
     }
   }
 
-  private File process(File test, File result, boolean updateFields) throws IOException {
+  private File process(File test, File result, boolean updateFields, String appendDocs) throws IOException {
     AsposeWordsTask task = new AsposeWordsTask();
     task.setSrc(new File(test, test.getName() + ".docx"));
     task.setDest(result);
@@ -79,6 +93,7 @@ public class AsposeWordsTaskTest {
       if (updateFields) {
         task.setUpdateFields(true);
       }
+      task.setAppendDocs(appendDocs);
     } catch (FileNotFoundException ex) {
       throw new IOException("The clientid and clientsecret must be defined in pso-aspose-ant/aspose.properties");
     }
